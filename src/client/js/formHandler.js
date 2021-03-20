e62e977d2d83059374965d8b6693e940967da7e9
bayomi <bmeabeed@gmail.com>
2021-03-20 09:49:03 +0200

    show different json attributes

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,6 @@
-
+function returnRow(varName,value){
+    return "<tr><td>"+varName+"</td><td>"+value+"</td></tr>"
+}
 
 function handleSubmit(event) {
     event.preventDefault()
@@ -9,16 +11,31 @@ function handleSubmit(event) {
        alert("Invaled Url");
        return;
    }
-   document.getElementById('nlpRender').value="loading"
+   document.getElementById('nlpRender').innerHTML="loading"
   
    // Fetch NLP json for the given url
    fetch('http://localhost:8080/nlp?url='+url)
     .then(res => res.json())
   .then(function(res) {
-    
-    
+   
+   
+    let returnData="<table>"
+
+
+
+
+    returnData+=returnRow("score_tag",res.score_tag)
+    returnData+=returnRow("confidence",res.confidence)
+    returnData+=returnRow("subjectivity",res.subjectivity)
+    returnData+=returnRow("agreement",res.agreement)
+    returnData+=returnRow("irony",res.irony)
+
+    returnData+="</table>"
+
+   //alert(returnData)
+
     //render the return NLP json object inside html  TEXTAREA tag nlpRender
-    document.getElementById('nlpRender').value= JSON.stringify(res,undefined, 4);
+     document.getElementById('nlpRender').innerHTML= returnData;
     
    
     
